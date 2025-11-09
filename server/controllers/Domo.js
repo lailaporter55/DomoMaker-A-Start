221507340f9098426133f53444418d4a02b1677f
lailaporter55 <lmp3003@rit.edu>
2025-11-09 12:00:14 -0500

    makerPage

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -33,5 +33,31 @@ DomoSchema.statics.toAPI = (doc) => ({
     age: doc.age,
 });
 
+const makeDomo = async (req, res) => {
+    if(!req.body.name || !req.body.age){
+        return res.status(400).json({error: 'Both name and age are required!'}); 
+    }
+    const domoData = {
+        name: req.body.name, 
+        age: req.bady.age, 
+        owner: req.session.account._id, 
+    }; 
+    try{
+        const newDomo = new Domo(domoData); 
+        await newDomo.save(); 
+        return res.json({redirect: '/maker'});
+    } catch(err){
+        console.log(err); 
+        if(err.code === 11000){
+            return res.status(400).json({error: 'Domo already exist!'}); 
+        }
+        return res.status(500).json({error: 'An error occured making domo!'}); 
+    }
+}
+
 const DomoModel = mongoose.model('Domo', DomoSchema); 
-module.exports = DomoModel; 
+module.exports = {
+    DomoModel, 
+    makerPage, 
+    makeDomo, 
+}
